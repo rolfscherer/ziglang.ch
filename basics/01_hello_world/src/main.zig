177aa80da0b2405d4379948090ef90d74f9750de
Rolf <rolf@scherer.ml>
2023-04-25 15:07:40 +0200

    Some cleanups

--- basics/01_hello_world/src/main.zig
+++ basics/01_hello_world/src/main.zig
@@ -1,19 +1,19 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Build and run the program via build.zip:
+    // Build and run the program via build.zig:
     // zig build run
 
     // Print "Hello, World" to stdout unbufferd.
-    const stdout_file = std.io.getStdOut().writer();
-    try stdout_file.print("Hello, World\n", .{});
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Hello, World\n", .{});
 
     // Print "Hello again" to stdout bufferd.
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-    try stdout.print("Hello again\n", .{});
+    var bw = std.io.bufferedWriter(stdout);
+    const stdout_writer = bw.writer();
+    try stdout_writer.print("Hello again\n", .{});
 
-    // flush the buffer
+    // Flush the buffer
     try bw.flush();
 }
 
